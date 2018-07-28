4feec0b5d22da2dbc89db2fed1a24eb1bf3f0301
cyberx4 <41820764+cyberx4@users.noreply.github.com>
2018-07-28 12:28:50 +0200

    Add System.Configuration.ConfiguratonManger
    ConnectionString

---
+++ Stock/Stock/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Stock
+{
+    public static class Connection
+    {
+        public static SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["StockConn"].ConnectionString;
+            return con;
+
+        }
+     
+    }
+}
